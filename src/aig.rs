875339c1541eba9ccf8d8db7a5076d4b7cafb73c
Konstantin Chukharev <lipen00@gmail.com>
2023-06-20 14:51:04 +0400

    Make Aig constructor public, add asserts

--- src/aig.rs
+++ src/aig.rs
@@ -12,12 +12,16 @@ pub struct Aig {
 }
 
 impl Aig {
-    pub(crate) fn new(
+    pub fn new(
         inputs: Vec<AigInput>,
         outputs: Vec<Ref>,
         and_gates: Vec<AigAndGate>,
         mapping: HashMap<u32, Node>,
     ) -> Self {
+        assert_eq!(inputs.len() + and_gates.len(), mapping.len());
+        assert!(inputs.iter().all(|input| mapping.contains_key(&input.id)));
+        assert!(outputs.iter().all(|output| mapping.contains_key(&output.id())));
+        assert!(and_gates.iter().all(|gate| mapping.contains_key(&gate.id)));
         Self {
             inputs,
             outputs,
