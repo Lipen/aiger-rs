52861e5e023b5940254c18e40d4cecbb449a725c
Konstantin Chukharev <lipen00@gmail.com>
2023-06-22 00:01:11 +0400

    Add `Aig::eval`

--- src/aig.rs
+++ src/aig.rs
@@ -114,6 +114,34 @@ impl Aig {
     }
 }
 
+impl Aig {
+    pub fn eval(&self, input_values: Vec<bool>) -> HashMap<u32, bool> {
+        assert_eq!(input_values.len(), self.inputs.len());
+
+        let mut values: HashMap<u32, bool> = HashMap::new();
+
+        for layer in self.layers_input() {
+            for id in layer {
+                match self.node(id) {
+                    Node::Input(input) => {
+                        let i = self.inputs.iter().position(|&x| x == input).unwrap();
+                        let value = input_values[i];
+                        values.insert(id, value);
+                    }
+                    Node::AndGate(gate) => {
+                        let left = values[&gate.args[0].id()] ^ gate.args[0].is_negated();
+                        let right = values[&gate.args[1].id()] ^ gate.args[1].is_negated();
+                        let value = left && right;
+                        values.insert(id, value);
+                    }
+                }
+            }
+        }
+
+        values
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -153,5 +181,40 @@ mod tests {
         assert_eq!(layers_output[1], vec![5]);
         assert_eq!(layers_output[2], vec![3, 4]);
         assert_eq!(layers_output[3], vec![1, 2]);
+    }
+
+    #[test]
+    fn test_eval() {
+        let x1 = AigInput { id: 1 };
+        let x2 = AigInput { id: 2 };
+        let x3 = AigInput { id: 3 };
+        // g1 = x1 and x2
+        let g1 = AigAndGate {
+            id: 4,
+            args: [Ref::positive(x1.id), Ref::positive(x2.id)],
+        };
+        // g2 = ~g1 and x3
+        let g2 = AigAndGate {
+            id: 5,
+            args: [Ref::negative(g1.id), Ref::positive(x3.id)],
+        };
+        // g3 = x1 and ~g2
+        let g3 = AigAndGate {
+            id: 6,
+            args: [Ref::positive(x1.id), Ref::negative(g2.id)],
+        };
+        let inputs = vec![x1, x2, x3];
+        let outputs = vec![Ref::positive(g3.id)];
+        let gates = vec![g1, g2, g3];
+        let aig = Aig::new(inputs, outputs, gates);
+
+        let input_values = vec![true, false, true]; // [x1, x2, x3]
+        let values = aig.eval(input_values);
+        assert_eq!(values[&1], true); // x1
+        assert_eq!(values[&2], false); // x2
+        assert_eq!(values[&3], true); // x3
+        assert_eq!(values[&4], false); // g1 = x1 and x2
+        assert_eq!(values[&5], true); // g2 = ~g1 and x3
+        assert_eq!(values[&6], false); // g3 = x1 and ~g2
     }
 }
